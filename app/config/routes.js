a3f48b6fdc1a73d2ac7e0f1a9b8b297f2321237e
Bruno Viera <brunovierag@gmail.com>
2016-07-13 20:59:03 -0300

    new landpage

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,8 +6,8 @@ module.exports = function() {
     var router = express.Router();
 
     router.get('/', siteCtrl.getIndex);
-    router.get('/adoptar', siteCtrl.getAdoptar);
-    router.get('/perdidosyencontrados', siteCtrl.getPerdidosEncontrados);
+    // router.get('/adoptar', siteCtrl.getAdoptar);
+    // router.get('/perdidosyencontrados', siteCtrl.getPerdidosEncontrados);
     // router.get('/partials/:name', siteCtrl.getPartials);
     // router.post('/login', siteCtrl.login);
     // router.post('/registration', siteCtrl.registration);
